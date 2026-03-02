6663a7d84d561f8eb25a6665ad905bef02be9673
Marvin <marvinbernard2018@gmail.com>
2026-03-02 18:51:17 +0300

    chore: create superuser marvin and assign admin role

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Comment
+from .serializers import CommentSerializer
 
 # Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        dish_id = self.request.query_params.get('dish')
+        if dish_id:
+            queryset = queryset.filter(dish_id=dish_id)
+        return queryset
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user and self.request.user.role != 'admin':
+            raise PermissionDenied("You can only delete your own comments.")
+        instance.delete()
--- backend/dishes/views.py
+++ backend/dishes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Dish
+from .serializers import DishSerializer
 
 # Create your views here.
+class IsAdminRole(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.role == 'admin'
+
+class DishViewSet(viewsets.ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+    permission_classes = [IsAdminRole]
